6f5367fc3d2b329eb435b24c8c637c19c5cd6510
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-14 16:44:29 +0530

    XAMARIN-34264 - Sample and screen shot added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/FontSizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class FontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var level = (int)value;
+            if (level == 0)
+                return 20;
+            else if (level == 1)
+                return 17;
+            else
+                return 14;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
